f9613a1e2e83c7b6431be933c9cdb4205f79e3ed
Oliver Nordén <oliver@tapirens.se>
2019-09-25 17:08:42 +0200

    Quiz class that takes questions object as props

--- main.js
+++ main.js
@@ -1,3 +1,50 @@
+class Quiz {
+    constructor(questions) {
+        this.questions = questions;
+    }
+}
+
+const questions = [
+    {
+        question: "Question 1",
+        answers: [
+            {
+                answer: "Answer 1",
+                correct: false
+            },
+            {
+                answer: "Answer 2",
+                correct: true
+
+            },
+            {
+                answer: "Answer 3",
+                correct: false
+            }
+        ]
+    },
+    {
+        question: "Question 2",
+        answers: [
+            {
+                answer: "Answer 1",
+                correct: false
+            },
+            {
+                answer: "Answer 2",
+                correct: true
+
+            },
+            {
+                answer: "Answer 3",
+                correct: true
+            }
+        ]
+    }
+]
+
+let game = new Quiz(questions);
+
 document.addEventListener('DOMContentLoaded', () => {
     let hello = document.createElement('p');
     hello.textContent = 'Hello world';
