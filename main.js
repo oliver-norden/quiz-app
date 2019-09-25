a78ef8f3bae9be99ea2b297e280086b4c42563a6
Oliver Nordén <oliver@tapirens.se>
2019-09-25 18:10:45 +0200

    Method to correct quiz and return score

--- main.js
+++ main.js
@@ -6,6 +6,24 @@ class Quiz {
     toggleAnswerSelected(question, answer) {
         this.questions[question].answers[answer].selected = !this.questions[question].answers[answer].selected;
     }
+
+    correctQuiz() {
+        // Loop through questions and accumulate score
+        const quizScore = this.questions.reduce((quizScore, question) => {
+
+            // Loop through answers and accumulate score
+            const questionScore = question.answers.reduce((questionScore, answer) => {
+                const { correct, selected } = answer;
+                if (correct && selected) return questionScore + 1; // Correct answer selected
+                if (!correct && !selected) return questionScore; // Wrong answer not selected 
+                if (correct !== selected) return questionScore - 1; // Wrong answer selected or correct answer not selected
+            }, 0);
+
+            return quizScore + questionScore;
+        }, 0);
+
+        return quizScore
+    }
 }
 
 const questions = [
@@ -49,8 +67,11 @@ const questions = [
 
 let game = new Quiz(questions);
 
-game.toggleAnswerSelected(0,0);
+game.toggleAnswerSelected(0,1);
+game.toggleAnswerSelected(1,1);
+game.toggleAnswerSelected(1,2);
 console.log(game.questions);
+alert(`Score: ${game.correctQuiz()}`);
 
 document.addEventListener('DOMContentLoaded', () => {
     let hello = document.createElement('p');
